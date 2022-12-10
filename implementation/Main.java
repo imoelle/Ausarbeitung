e62bb1ad712c02eccf7d418186287044a18498cf
Ingo <73609246+imoelle@users.noreply.github.com>
2022-12-10 13:25:46 +0100

    create rectangle class

--- implementation/Main.java
+++ implementation/Main.java
@@ -1,6 +1,13 @@
+import geometrics.Rectangle;
+import utilities.Vector2D;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Semesterbegleitende Ausarbeitung: Aufgabe 4 - Design Patterns der OOP");
+
+        Vector2D v1 = new Vector2D(2, 1);
+        Vector2D v2 = new Vector2D(6, 3);
+        Rectangle r1 = new Rectangle(v1, v2);
 
     }
 }
